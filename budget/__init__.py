d408f4c12c619bb79534a480354a5efab96fb652
Arthur RICHARD <arthur.richard@protonmail.com>
2020-08-17 17:20:27 +0200

    Pre-setup init.py

--- budget/__init__.py
+++ budget/__init__.py
@@ -1,15 +1,18 @@
-from .parser import get_parsed_data
-from .parser import print_raw_data
-from .query import get_data_by_year
-from .query import get_total_expenses
-from .query import get_total_incomes
-from .query import get_years
+import os
 
-__all__ = [
-    "get_parsed_data",
-    "print_raw_data",
-    "get_data_by_year",
-    "get_total_expenses",
-    "get_total_incomes",
-    "get_years"
-]
+if os.path.exists(os.path.join(os.path.dirname(__file__), "transactions.csv")):
+    from .parser import get_parsed_data
+    from .parser import print_raw_data
+    from .query import get_data_by_year
+    from .query import get_total_expenses
+    from .query import get_total_incomes
+    from .query import get_years
+
+    __all__ = [
+        "get_parsed_data",
+        "print_raw_data",
+        "get_data_by_year",
+        "get_total_expenses",
+        "get_total_incomes",
+        "get_years"
+    ]
