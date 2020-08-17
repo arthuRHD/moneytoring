7f964006e33b4682e0debe7bd428cb6c230149bd
Arthur RICHARD <arthur.richard@protonmail.com>
2020-08-17 17:29:27 +0200

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="budget",  # Replace with your own username
-    version="0.1.0",
+    version="0.2.0",
     author="Arthur RICHARD",
     author_email="arthur.richard@protonmail.com",
     description="A python budgeting tool",
